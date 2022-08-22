decdc5c6d198b78e42bbf02325492abffcf85831
nandweza <allannandweza@gmail.com>
2022-08-22 18:51:10 +0300

    Create a new user

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import { Express } from 'express';
 import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
 
 /**
  * Injects routes with their handlers to the given Express application.
@@ -9,6 +10,8 @@ import AppController from '../controllers/AppController';
 const injectRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
+
+  api.post('/users', UsersController.postNew);
 };
 
 export default injectRoutes;
